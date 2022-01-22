805d44bf0968b96a8ee7c546dca3975743aad3ee
Nabil Ould Hamou <ouldhamounabil@gmail.com>
2022-01-22 15:02:50 +0100

    Added the tilemap system and a temporary class called tempLocation for testing purposes.

--- core/src/com/umbrella/game/Main.java
+++ core/src/com/umbrella/game/Main.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.umbrella.game.client.maps.GameMap;
 import com.umbrella.game.utils.tasks.InputThread;
 import com.umbrella.game.utils.tasks.ThreadsManager;
 
@@ -11,19 +12,23 @@ public class Main extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	ThreadsManager tasksManager;
+	GameMap gameMap;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("img/damn.jpeg");
 		tasksManager = new ThreadsManager(new InputThread());
+
+		gameMap = new GameMap(new int[2][2]);
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
+		gameMap.render();
 		batch.begin();
-		batch.draw(img, 0, 0, 700,500);
+		//batch.draw(img, 0, 0, 700,500);
 		batch.end();
 	}
 	
