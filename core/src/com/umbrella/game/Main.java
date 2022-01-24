50460dc4909319c85bdee627906592e2adfc9898
Ben GREGORY <bengregorygbr@gmail.com>
2022-01-24 19:13:01 +0100

    called the gameloop method in the render loop

--- core/src/com/umbrella/game/Main.java
+++ core/src/com/umbrella/game/Main.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.umbrella.game.client.maps.GameMap;
+import com.umbrella.game.manager.GameManager;
 import com.umbrella.game.object.GameObject;
 import com.umbrella.game.client.maps.MapRenderer;
 import com.umbrella.game.utils.tasks.InputThread;
@@ -30,7 +31,7 @@ public class Main extends ApplicationAdapter {
 	public void render () {
 		ScreenUtils.clear(0, 0, 0, 1);
 		mapRenderer.render();
-
+		GameManager.getInstance().gameLoop();
 		//go.render();
 	}
 	
